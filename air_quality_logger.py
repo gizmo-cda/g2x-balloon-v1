4ddfbb41563bdb7f1d327f548b0fba73418ac26a
Kevin Lindsey <kevin@kevlindev.com>
2018-08-17 13:50:22 -0700

    Rename airbeam logger script

--- air_quality_logger.py
+++ air_quality_logger.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+
 import time
 import serial
 import os
 import shutil
 import subprocess
 import json
+
 # Initialize variables.
 output_path = '/home/pi/app/output.csv'
 config_path = '/home/pi/app/settings.json'
@@ -19,8 +21,10 @@ ser = serial.Serial(
 
 # Grab and parse the settings.
 settings = "";
+
 with open(config_path, 'r') as settings_file:
 	settings = json.load(settings_file);
+
 if settings == "":
 	print("Malformed settings file")
 	exit(1)
@@ -29,19 +33,24 @@ counter = 0
 delay = int(settings["delay"])
 use_date = int(settings["date"])
 use_ntp = int(settings["ntp"])
+
 # Set initial date.
 if (use_date == 1) and (use_ntp == 1):
 	time.sleep(5)
 	subprocess.call(['/home/pi/sync_time.sh'])
+
 # Start measurement loop.
 while 1:
 	# Empty serial buffer.
 	ser.flushInput()
+
 	# Read data.
 	x=ser.readline()
+
 	# Parse data.
 	data = x.decode('UTF-8').strip().split(' ')
 	print(data)
+
 	# Check for good data and output to file.
 	if len(data) > 10:
 		with open(output_path, 'a') as output:
@@ -52,9 +61,12 @@ while 1:
 				output.write(str(timepoint) + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] + "," + data[9] + "," + data[10] + "\r\n")
 		# Copy output to server directory.
 		#shutil.copy(output_path, server_path)
+
 	counter += 1
+
 	# Syncy date and time via NTP every 24 hours if needed.
 	if (use_date == 1) and (use_ntp == 1) and (counter * delay > 86400):
 		counter = 0
 		subprocess.call(['/home/pi/sync_time.sh'])
+
 	time.sleep(delay);
