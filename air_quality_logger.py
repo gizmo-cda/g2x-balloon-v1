c64c25f6d72397bbcfcd54d18038814c81c457f6
Kevin Lindsey <kevin@kevlindev.com>
2018-08-17 14:18:07 -0700

    Simplify code
    We no longer use the settings.json file, so that has been deleted

--- air_quality_logger.py
+++ air_quality_logger.py
@@ -1,72 +1,35 @@
 #!/usr/bin/env python
 
+import os
 import time
 import serial
-import os
-import shutil
-import subprocess
-import json
 
-# Initialize variables.
-output_path = '/home/pi/app/output.csv'
-config_path = '/home/pi/app/settings.json'
+# configuration
+delay = 5
+timestamp = int(time.time())
+output_path = filename = "g2x-balloon-{}.csv".format(timestamp)
+
+# open serial connection
 ser = serial.Serial(
-	port='/dev/ttyACM0',
-	baudrate = 9600,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS,
-	timeout=1
+    port='/dev/ttyACM0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
 )
 
-# Grab and parse the settings.
-settings = "";
-
-with open(config_path, 'r') as settings_file:
-	settings = json.load(settings_file);
-
-if settings == "":
-	print("Malformed settings file")
-	exit(1)
-
-counter = 0
-delay = int(settings["delay"])
-use_date = int(settings["date"])
-use_ntp = int(settings["ntp"])
-
-# Set initial date.
-if (use_date == 1) and (use_ntp == 1):
-	time.sleep(5)
-	subprocess.call(['/home/pi/sync_time.sh'])
-
-# Start measurement loop.
+# read data forever
 while 1:
-	# Empty serial buffer.
-	ser.flushInput()
+    ser.flushInput()
+    text = ser.readline()
+    data = text.decode('UTF-8').strip().split(' ')
 
-	# Read data.
-	x=ser.readline()
+    if len(data) > 10:
+        with open(output_path, 'a') as output:
+            output.write("{},{}\r\n".format(
+                time.strftime("%Y/%m/%d %H:%M:%S"),
+                join(",", data[2:10])
+            ))
 
-	# Parse data.
-	data = x.decode('UTF-8').strip().split(' ')
-	print(data)
-
-	# Check for good data and output to file.
-	if len(data) > 10:
-		with open(output_path, 'a') as output:
-			if use_date == 1:
-				output.write(time.strftime("%Y/%m/%d %H:%M:%S" + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] + "," + data[9] + "," + data[10] + "\r\n"))
-			else:
-				timepoint = (counter * delay) / 60
-				output.write(str(timepoint) + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] + "," + data[9] + "," + data[10] + "\r\n")
-		# Copy output to server directory.
-		#shutil.copy(output_path, server_path)
-
-	counter += 1
-
-	# Syncy date and time via NTP every 24 hours if needed.
-	if (use_date == 1) and (use_ntp == 1) and (counter * delay > 86400):
-		counter = 0
-		subprocess.call(['/home/pi/sync_time.sh'])
-
-	time.sleep(delay);
+    time.sleep(delay);
