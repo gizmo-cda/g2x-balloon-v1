18a4103345d72ef4e1a8a0f8c81915f2ad1bc992
Kevin Lindsey <kevin@kevlindev.com>
2018-09-14 18:32:54 -0700

    Fix N calculation error and format things a bit

--- wgs84-to-ecef.py
+++ wgs84-to-ecef.py
@@ -5,15 +5,18 @@
 
 from math import *
 
-# some constants
-semi_major_axis = 6378137
-semi_minor_axis = 6356752.314245
-aa = semi_major_axis * semi_major_axis
-ee = 1.0 - semi_minor_axis**2 / semi_major_axis**2  # first eccentricy squared
+# major/minor axes in meters
+semi_major_axis = 6_378_137
+semi_minor_axis = 6_356_752.314_245
+
+# first eccentricy squared
+ee = 1.0 - semi_minor_axis**2 / semi_major_axis**2
 
 
 def prime_vertical_radius_of_curvature(latitude_radians):
-    return semi_major_axis / sqrt(1.0 - ee * sin(latitude_radians))
+    sin_lat = sin(latitude_radians)
+
+    return semi_major_axis / sqrt(1.0 - ee * sin_lat**2)
 
 
 def wgs84_to_ecef(latitude_degrees, longitude_degrees, height_meters):
@@ -24,28 +27,33 @@ def wgs84_to_ecef(latitude_degrees, longitude_degrees, height_meters):
     return (
         (N + height_meters) * cos(latitude_radians) * cos(longitude_radians),
         (N + height_meters) * cos(latitude_radians) * sin(longitude_radians),
-        ((1 - ee) * N + height_meters) * sin(latitude_radians)
+        ((1.0 - ee) * N + height_meters) * sin(latitude_radians)
     )
 
+
 if __name__ == "__main__":
-    meters_per_foot = 0.3048
-
-    cda_lat = 47.6776800
-    cda_long = -116.7804700
-    cda_height = 2191 * meters_per_foot
-
-    p1 = wgs84_to_ecef(cda_lat, cda_long, cda_height)
-    p2 = wgs84_to_ecef(cda_lat, cda_long, cda_height + 1)
-
     from operator import sub
-    normal = tuple(map(sub, p2, p1))
-    length = sqrt(normal[0]**2 + normal[1]**2 + normal[2]**2)
 
-    # I need one vector on plane
-    # then that vector * normal will give another
-    # vectors on plane are orthogonal
+    meters_per_foot = 0.304_800
 
-    print("p1 =", p1)
-    print("p2 =", p2)
-    print("normal =", normal)
-    print("length =", length)
+    for height in (0.0, 2191.0, 100_000.0):
+        cda_lat = 47.677_680_000
+        cda_long = -116.780_470_000
+        cda_height = height * meters_per_foot
+
+        p1 = wgs84_to_ecef(cda_lat, cda_long, cda_height)
+        p2 = wgs84_to_ecef(cda_lat, cda_long, cda_height + 1.0)
+
+        normal = tuple(map(sub, p2, p1))
+        length = sqrt(normal[0]**2 + normal[1]**2 + normal[2]**2)
+
+        # I need one vector on plane
+        # then that vector cross normal will give another vector
+        # these vectors on plane will be orthogonal
+
+        print("height =", cda_height)
+        print("p1 =", p1)
+        print("p2 =", p2)
+        print("normal =", normal)
+        print("length =", length)
+        print()
