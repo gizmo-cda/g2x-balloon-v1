e71c7c4f9644afd661a8b46abc159303cd7a5717
Kevin Lindsey <kevin@kevlindev.com>
2018-10-09 10:44:08 -0700

    More coordinate change experimentation

--- wgs84-to-ecef.py
+++ wgs84-to-ecef.py
@@ -4,6 +4,7 @@
 # convert WGS84 to ECEF
 
 from math import *
+from operator import add, mul, sub
 
 # major/minor axes in meters
 semi_major_axis = 6_378_137
@@ -19,7 +20,8 @@ def prime_vertical_radius_of_curvature(latitude_radians):
     return semi_major_axis / sqrt(1.0 - ee * sin_lat**2)
 
 
-def wgs84_to_ecef(latitude_radians, longitude_radians, height_meters):
+def wgs84_to_ecef(wgs84):
+    (latitude_radians, longitude_radians, height_meters) = wgs84
     N = prime_vertical_radius_of_curvature(latitude_radians)
 
     return (
@@ -28,6 +30,51 @@ def wgs84_to_ecef(latitude_radians, longitude_radians, height_meters):
         ((1.0 - ee) * N + height_meters) * sin(latitude_radians)
     )
 
+
+def wgs84_to_enu(from_wgs84, to_wgs84):
+    from_ecef     = wgs84_to_ecef(from_wgs84)
+    to_ecef       = wgs84_to_ecef(to_wgs84)
+    vec           = (from_ecef[0] - to_ecef[0], from_ecef[1] - to_ecef[1], from_ecef[2] - to_ecef[2])
+    (phi, lam, _) = to_wgs84
+
+    print("from_wgs84 =", from_wgs84)
+    print("to_wgs84 =", to_wgs84)
+    print("from_ecef =", from_ecef)
+    print("to_ecef =", to_ecef)
+    print("vec =", vec)
+
+    t0 = (
+       -sin(lam),
+        cos(lam),
+        0
+    )
+    t1 = (
+       -sin(phi) * cos(lam),
+       -sin(phi) * sin(lam),
+        cos(phi)
+    )
+    t2 = (
+        cos(phi) * cos(lam),
+        cos(phi) * sin(lam),
+        sin(phi)
+    )
+
+    return (
+        dot_product(t0, vec),
+        dot_product(t1, vec),
+        dot_product(t2, vec)
+    )
+
+
+def dot_product(a, b):
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+
+
+def meters(feet):
+    return feet * 0.304_800
+
+
+# Experimental functions
 
 def point_on_surface(latitude_radians, longitude_radians):
     u = latitude_radians
@@ -40,38 +87,47 @@ def point_on_surface(latitude_radians, longitude_radians):
     )
 
 
+def normal_on_surface(latitude_radians, longitude_radians):
+    p3 = point_on_surface(latitude_radians, longitude_radians)
+
+    dx = 2 / (semi_major_axis * semi_major_axis)
+    dy = dx
+    dz = 2 / (semi_minor_axis * semi_minor_axis)
+
+    p4 = (
+        p3[0] * dx,
+        p3[1] * dy,
+        p3[2] * dz
+    )
+
+    length = sqrt(p4[0]**2 + p4[1]**2 + p4[2]**2)
+    
+    return (
+        p4[0] / length,
+        p4[1] / length,
+        p4[2] / length
+    )
+
+
 if __name__ == "__main__":
-    from operator import sub
+    # lat, long, height
+    cda = (
+        radians(47.677_680_000),
+        radians(-116.780_470_000),
+        meters(2_191)
+    )
+    balloon = (
+        radians(48),
+        radians(-116.780_470_000),
+        meters(100_000)
+    )
 
-    meters_per_foot = 0.304_800
+    p1 = wgs84_to_ecef(cda)
+    p2 = wgs84_to_ecef(balloon)
+    v1 = wgs84_to_enu(cda, balloon)
 
-    cda_lat = 47.677_680_000
-    cda_long = -116.780_470_000
-
-    cda_lat_radians = radians(cda_lat)
-    cda_long_radians = radians(cda_long)
-
-    p3 = point_on_surface(cda_lat_radians, cda_long_radians)
-    print("p3 =", p3)
+    print("height =", cda[2])
+    print("p1 =", p1)
+    print("p2 =", p2)
+    print("v1 =", v1)
     print()
-
-    for cda_height in (0.0, 2191.0, 100_000.0):
-        cda_height_meters = cda_height * meters_per_foot
-
-        p1 = wgs84_to_ecef(cda_lat_radians, cda_long_radians, cda_height_meters)
-        p2 = wgs84_to_ecef(cda_lat_radians, cda_long_radians, cda_height_meters + 1.0)
-
-
-        normal = tuple(map(sub, p2, p1))
-        length = sqrt(normal[0]**2 + normal[1]**2 + normal[2]**2)
-
-        # I need one vector on plane
-        # then that vector cross normal will give another vector
-        # these vectors on plane will be orthogonal
-
-        print("height =", cda_height)
-        print("p1 =", p1)
-        print("p2 =", p2)
-        print("normal =", normal)
-        print("length =", length)
-        print()
