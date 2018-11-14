ca1972882ca2284dc5cbfec5b80972a22ebfd6be
TidyData <mckweb@outlook.com>
2018-11-13 20:54:45 -0800

    new plane's unit vectors are normal to each other
    Signed-off-by: TidyData <mckweb@outlook.com>

--- relpos.py
+++ relpos.py
@@ -20,6 +20,32 @@ import numpy as _np
 import math as math
 import numpy as np
 #import json as _json
+#-------ripped from gps\wgs84-to-ecef.py--------
+
+# major/minor axes in meters
+semi_major_axis = 6_378_137  # m
+semi_minor_axis = 6_356_752.314_245  # m
+
+# first eccentricy squared
+ee = 1.0 - semi_minor_axis**2 / semi_major_axis**2
+
+
+def prime_vertical_radius_of_curvature(latitude_radians):
+    sin_lat = _math.sin(latitude_radians)
+
+    return semi_major_axis / _math.sqrt(1.0 - ee * sin_lat**2)
+
+
+def wgs84_to_ecef(wgs84):
+    (latitude_radians, longitude_radians, height_meters) = wgs84
+    N = prime_vertical_radius_of_curvature(latitude_radians)
+
+    return (
+        (N + height_meters) * _math.cos(latitude_radians) * _math.cos(longitude_radians),
+        (N + height_meters) * _math.cos(latitude_radians) * _math.sin(longitude_radians),
+        ((1.0 - ee) * N + height_meters) * _math.sin(latitude_radians)
+    )
+#-------ripped from gps\wgs84-to-ecef.py--------
 
 class latlon(_typing.NamedTuple):
     deg: float
@@ -34,10 +60,66 @@ class wgs84tup(_typing.NamedTuple):
     longitude_rad: float
     elevation_ft: float
 
-class xyztup(_typing.NamedTuple):
+class xyz(_typing.NamedTuple):
     x: float
     y: float
     z: float
+
+class out_look_at(_typing.NamedTuple):
+    angle_rad_between_unit_vectors: float
+    unit_vector_axis_of_rotation: xyz
+    unit_vector_to_target: xyz
+
+def delta_xyz(curr_xyz, targ_xyz):
+    delta_x = float(targ_xyz.x - curr_xyz.x)
+    delta_y = float(targ_xyz.y - curr_xyz.y)
+    delta_z = float(targ_xyz.z - curr_xyz.z)
+    return xyz(x=delta_x, y=delta_y, z=delta_z)
+
+def unit_vector_to_target(current_pos, target_pos):
+    deltas = delta_xyz(curr_xyz=current_pos, targ_xyz=target_pos)
+    print(f'deltas.x: {deltas.x:.5}')
+    print(f'deltas.y: {deltas.y:.5}')
+    print(f'deltas.z: {deltas.z:.5}')
+    maxval = max(deltas)
+    return xyz(x=deltas.x / maxval, y=deltas.y / maxval, z=deltas.z / maxval)
+
+def dot_product(vect1, vect2):
+    dot_sum = 0
+    if len(vect1) != len(vect2) or len(vect1) == 0:
+        raise IOError('something wrong')
+    for (ax1, ax2) in zip(vect1, vect2):
+        dot_sum += ax1 * ax2
+    return dot_sum
+
+def cross_product(A, B):
+    """
+    Both the two operands and the result of the cross product are vectors.
+    The vector cross product has some useful properties, it produces a vector which is mutually perpendicular to the two vectors being multiplied.
+    The vector cross product gives a vector which is perpendicular to both the vectors being multiplied. The resulting vector A × B is defined by:
+    x = Ay * Bz - By * Az
+    y = Az * Bx - Bz * Ax
+    z = Ax * By - Bx * Ay
+    where x,y and z are the components of A x B
+    """
+    x = A.y * B.z - B.y * A.z
+    y = A.z * B.x - B.z * A.x
+    z = A.x * B.y - B.x * A.y
+    return xyz(x=x, y=y, z=z)
+
+def look_at(current_pos, target_pos, current_view_unit=xyz(x=1, y=0, z=0)):
+    ntarget = unit_vector_to_target(
+        current_pos=current_pos, target_pos=target_pos, )
+    dot_mag = dot_product(vect1=ntarget, vect2=current_view_unit)
+    angle_rad_between_unit_vectors = _math.acos(dot_mag)
+    print(f'rot-deg: {angle_rad_between_unit_vectors * 180 / _math.pi:.5}')
+
+    rot_vector = cross_product(A=ntarget, B=current_view_unit)
+    return out_look_at(
+        angle_rad_between_unit_vectors=angle_rad_between_unit_vectors,
+        unit_vector_axis_of_rotation=rot_vector,
+        unit_vector_to_target=ntarget,
+    )
 
 def conv_deghms_2_radians(deg, minute, second, hemisphere):
     r"""
@@ -118,6 +200,20 @@ def rotationMatrixToEulerAngles(R):
  
     return np.array([x, y, z])
 
+def gps_loc_plane_unit_vectors(gps_location):
+    rotation_matrix = eulerAnglesToRotationMatrix(theta=[
+        0,
+        gps_location.latitude_rad,  # latitude rotation about y
+        gps_location.longitude_rad,  # longitude rotation about z
+    ])
+    # unit_look_vectors
+    plane_x_unit_abs = rotation_matrix @ np.array(xyz(x=1, y=0, z=0))
+    plane_y_unit_abs = rotation_matrix @ np.array(xyz(x=0, y=1, z=0))
+    plane_z_unit_abs = rotation_matrix @ np.array(xyz(x=0, y=0, z=1))
+    return xyz(x=plane_x_unit_abs, y=plane_y_unit_abs, z=plane_z_unit_abs)
+
+def gps_loc_plane_unit_vectors(gps1: wgs84tup, gps2: wgs84tup):
+    delta_xyz(curr_xyz, targ_xyz)
 
 if __name__ == '__main__':
     # center parking line north cement meridian end barrier
@@ -142,4 +238,7 @@ if __name__ == '__main__':
 
     rot_matrix = eulerAnglesToRotationMatrix(theta=[_math.pi/2,_math.pi/2,0])
     rotationMatrixToEulerAngles(R=rot_matrix)
-    xyztup(x=1,y=1,z=1)
+    xyz(x=1,y=1,z=1)
+    
+    new_units = gps_loc_plane_unit_vectors(gps_location=gps1)
+    
