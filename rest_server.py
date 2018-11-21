16c3b99c186e481c9f0a9e333a18aafaea3c91c1
TidyData <mckweb@outlook.com>
2018-11-20 20:47:51 -0800

    merged rest_server and twogpspoints state machine into rest_server.py
    Now rest_server.py able to communicate and relpos is able to do the math, just need to make em use each other
    
    Signed-off-by: TidyData <mckweb@outlook.com>

--- rest_server.py
+++ rest_server.py
@@ -1,4 +1,23 @@
 r"""
+rest server module to interact with Google Earth and raspberry pi's.
+
+Composed of a general object to the two points "states".  States represent
+last updated position information from the raspberry pi's received through
+posts.
+
+Additionally composed of rest server that manages the http responses
+accordingly.
+
+* note on surface normal: http://www.geo.hunter.cuny.edu/~jochen/GTECH201/Lectures/Lec6concepts/05%20-%20Understanding%20datums.html
+* good article on wg84 history: https://frontierprecision.com/wp-content/uploads/EvolutionofWGS84andNAD83.pdf
+
+
+* gps coordinates, guide to google maps verification: https://www.ubergizmo.com/how-to/read-gps-coordinates/
+
+The simplest way to transform coordinates in Python is pyproj, i.e. the
+Python interface to PROJ.4 library. https://gis.stackexchange.com/questions/78838/converting-projected-coordinates-to-lat-lon-using-python/78944#78944
+
+
 Want to be able to handle GET & POST requests.  So following
 [Simple Python HTTP(S) Server With GET/POST Examples](https://blog.anvileight.com/posts/simple-python-http-server/).
 
@@ -25,16 +44,104 @@ or using only python
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 from io import BytesIO
+import pathlib as _pathlib
+import inspect as _inspect
 import json as _json
 
-import twogpspoints   # import TwoGps
+
+import relpos as _relpos
 import pdb
 
-PORT = 8000
+def write_move_gps_state(fp_abs, gps):
+    """
+    Moves are atomic, writing a file is not.  Want to write to temp file and
+    then move the file to the final file path absolute location.
+    refer to https://realpython.com/python-pathlib/#reading-and-writing-files
+
+    JSON + namedtuple
+    https://stackoverflow.com/questions/28148260/writing-and-reading-namedtuple-into-a-file-in-python/28149664#28149664
+
+    Interesting module but KISS for this instance
+    https://github.com/ltworf/typedload
+    """
+    tmp_fp_abs = fp_abs.parent / f'tmp-{fp_abs.name:s}'
+    state_text = _json.dumps(gps._asdict())  # uses ordered dict
+    tmp_fp_abs.write_text(state_text)
+    tmp_fp_abs.replace(fp_abs)  # deletes the tmp file
+
+def read_gps_state(fp_abs):
+    state_text = fp_abs.read_text()
+    gps_pt = _relpos.wgs84tup(**_json.loads(state_text))
+    return gps_pt
+
+def kml_byte_str(gps1, gps2, ):
+    """
+    writes the KML Google Earth displays
+    """
+    #gps1 = latitude_rad, longitude_rad, elevation_m
+
+    kml = (
+       '<?xml version="1.0" encoding="UTF-8"?>\n'
+       '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
+       '<Placemark>\n'
+       '<name>Random Placemark</name>\n'
+       '<Point>\n'
+       '<coordinates>%d,%d</coordinates>\n'
+       '</Point>\n'
+       '</Placemark>\n'
+       '</kml>'
+       ) % (gps1.longitude_rad, gps1.latitude_rad)
+    return kml
+
+class TwoGps(object):
+    def __init__(self, state_dir=None, gps1=None, gps2=None):
+        if state_dir is not None:
+            self.state_dir = _pathlib.Path(state_dir)
+        else:
+            # modern py3.4+ way to get the __file__ directory
+            # when running from site-packages can get irradic behavior
+            # https://stackoverflow.com/questions/3718657/how-to-properly-determine-current-script-directory
+            filename = _inspect.getframeinfo(_inspect.currentframe()).filename
+            self.state_dir = _pathlib.Path(filename).resolve().parent
+            self.state_dir /= 'state_dir'
+
+        if not self.state_dir.exists() and self.state_dir.parent.exists():
+            self.state_dir.mkdir(parents=False, exist_ok=True)
+        self.gps1_state_fpabs = self.state_dir / 'gps1_state.tsv'
+        self.gps2_state_fpabs = self.state_dir / 'gps2_state.tsv'
+        if gps1 is None:
+            gps1 = _relpos.wgs84tup(latitude_rad=0., longitude_rad=0., elevation_m=0.)
+        self.gps1 = gps1
+        if gps2 is None:
+            gps2 = _relpos.wgs84tup(latitude_rad=0., longitude_rad=0., elevation_m=0.)
+        self.gps2 = gps2
+
+
+    @property  # https://www.programiz.com/python-programming/property
+    def gps1(self):
+        return read_gps_state(fp_abs=self.gps1_state_fpabs)
+
+    @property  # https://www.programiz.com/python-programming/property
+    def gps2(self):
+        return read_gps_state(fp_abs=self.gps2_state_fpabs)
+
+    @gps1.setter  # https://www.programiz.com/python-programming/property
+    def gps1(self, value):
+        write_move_gps_state(fp_abs=self.gps1_state_fpabs, gps=value)
+
+    @gps2.setter  # https://www.programiz.com/python-programming/property
+    def gps2(self, value):
+        write_move_gps_state(fp_abs=self.gps2_state_fpabs, gps=value)
+
+    def gen_kml_str(self):
+        byte_str = kml_byte_str(gps1=self.gps1, gps2=self.gps2, )
+        return byte_str
+
+
 
 class StoreHandler(BaseHTTPRequestHandler):
     #store_path = pjoin(curdir, 'store.json')
-    two_gps = twogpspoints.TwoGps()
+    two_gps = TwoGps()
     # kml_bytes = two_gps.gen_kml_str().encode()
 
     def do_GET(self):
@@ -79,6 +186,35 @@ class StoreHandler(BaseHTTPRequestHandler):
 #        response.write(body)
 #        self.wfile.write(response.getvalue())
 
+if True and __name__ == '__main__':
+    # center parking line north cement meridian end barrier
+    lat1 = _relpos.latlon(deg=47, minute=40, second=32.63, hemisphere='N')
+    lon1 = _relpos.latlon(deg=116, minute=47, second=43.07, hemisphere='W')
+    elevation_m1 = 0.3048 * 2139  # ft/m * ft
+    _gps1 = _relpos.wgs84tup(
+        latitude_rad=_relpos.conv_deghms_2_radians(**lat1._asdict()),
+        longitude_rad=_relpos.conv_deghms_2_radians(**lon1._asdict()),
+        elevation_m=elevation_m1,
+    )
+
+    # center parking line south cement meridian end barrier farthest east point
+    lat2 = _relpos.latlon(deg=47, minute=40, second=30.93, hemisphere='N')
+    lon2 = _relpos.latlon(deg=116, minute=47, second=42.54, hemisphere='W')
+    elevation_m2 = 0.3048 * 2140  # ft/m * ft
+    _gps2 = _relpos.wgs84tup(
+        latitude_rad=_relpos.conv_deghms_2_radians(**lat2._asdict()),
+        longitude_rad=_relpos.conv_deghms_2_radians(**lon2._asdict()),
+        elevation_m=elevation_m2,
+    )
+
+    tgps = TwoGps(gps1=_gps1, gps2=_gps2)
+    lat1_rad = _gps1.latitude_rad
+    lon1_rad = _gps1.longitude_rad
+    lat2_rad = _gps2.latitude_rad
+    lon2_rad = _gps2.longitude_rad
+
+
+PORT = 8000
 if False:
     httpd = HTTPServer(('localhost', PORT), StoreHandler)
     httpd.serve_forever()
@@ -92,15 +228,6 @@ elif True and __name__ == '__main__':
         print("serving at port", PORT)
         httpd.serve_forever(poll_interval=0.5)
 
-#from os import curdir
-#from os.path import join as pjoin
-#
-#from http.server import BaseHTTPRequestHandler, HTTPServer
-#
-#
-#server = HTTPServer(('', 8080), StoreHandler)
-#server.serve_forever()
-#
 #$ curl -X POST --data "one two three four" localhost:8080/store.json
 #$ curl -X GET localhost:8080/store.json
 #one two three four%
